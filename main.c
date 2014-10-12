5529bf696b581e953e0217c248687c5d714d6e9f
Gareth Pulham <garethpulham@gmail.com>
2014-10-12 20:16:25 +0100

    Add homura, make bg not shit colour

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
@@ -54,13 +55,34 @@ int main(int argc, char* argv[]){
 
     // We have a valid window now. Lets load the surfaces for window and image
     screenSurf = SDL_GetWindowSurface(window);
-    testImage = loadSurface("test_mask.png", screenSurf->format);
+    testImage = loadSurface("homura.png", screenSurf->format);
 
     // Now lets color and write
-    SDL_FillRect(screenSurf, 0, SDL_MapRGB(screenSurf->format, 0xFF, 0x00, 0xFF));
+    SDL_FillRect(screenSurf, 0, SDL_MapRGB(screenSurf->format, 0xC8, 0xA2, 0xC8)); // Lilac
     SDL_BlitSurface(testImage, NULL, screenSurf, NULL);
     SDL_UpdateWindowSurface(window);
-    SDL_Delay(5000);
+
+    // Loop and handle events
+    SDL_Event event;
+    bool running = true;
+    while (running){
+        while (SDL_PollEvent(&event) != 0){
+            if (event.type == SDL_QUIT){
+                running = false;
+            }
+            else if (event.type == SDL_KEYDOWN){
+                switch (event.key.keysym.sym) {
+                    case SDLK_q:
+                        running = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else {}
+        }
+        SDL_Delay(30);  // About 30 FPS
+    }
 
     SDL_DestroyWindow(window);
     SDL_Quit();
